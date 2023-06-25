6025e27982b1378f19c7fad78f73eb30c63853b9
Jhansi <95856797+jhans1-cs@users.noreply.github.com>
2023-06-25 19:50:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ const output = document.getElementById("output");
 //your code here
 async function showMessage() {
   const message = text.value;
-  const delayVal = delay.value;
-  await new Promise((resolve) => setTimeout(resolve, delayVal));
-  output.innerText = message;
+  const delayVal = parseInt(delay.value);
+
+  if (!isNaN(delayVal)) {
+    await new Promise((resolve) => setTimeout(resolve, delayVal));
+    output.innerText = message;
+  } 
 }
 
 btn.addEventListener("click", showMessage);
